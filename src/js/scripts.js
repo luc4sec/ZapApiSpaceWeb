1ddaeba240dfd0f1f1b7a7cb417ed242a232a099
Lucas Araújo <ramaolucas01@gmail.com>
2022-02-18 18:30:11 -0300

    é isso

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,94 +1,9 @@
-let url;
-let host = '107.152.47.102'
-let host_ssl = '107.152.47.102'
-let port = '3333'
-url = "http://107.152.47.102:3333" 
-async function getSession(session) {
-    const config = {
-        headers: {
-            apitoken: document.getElementById("apitoken").value,
-            sessionkey: document.getElementById("sessionkey").value
-        }
-    }
-
-    const data = {
-        session: document.getElementById("session").value,
-        wh_status: '', //document.getElementById("wh_status").value,
-        wh_message: '', //document.getElementById("wh_message").value,
-        wh_qrcode: '', //document.getElementById("wh_qrcode").value,
-        wh_connect: '', //document.getElementById("wh_connect").value,
-    }
-    axios.post(url + "/start", data, config)
-        .then((value) => {})
-        .catch((err) => {})
-}
-
-async function alterSession(session) {
-    session = document.getElementById('session').value
-    if (!session) {
-        Swal.fire(
-            'Error!!',
-            'Digite o nome da sessão antes de continuar...',
-            'error'
-        )
-    } else
-    if (!document.getElementById('apitoken').value) {
-        Swal.fire(
-            'Error!!',
-            'Digite o TOKEN da API antes de continuar...',
-            'error'
-        )
-    } else
-    if (!document.getElementById('sessionkey').value) {
-        Swal.fire(
-            'Error!!',
-            'Digite a SESSION KEY da sessão antes de continuar...',
-            'error'
-        )
-    } else {
-        //document.getElementById('image').style.visibility = "visible";
-        document.getElementById('image').src = "./images/loading.gif"
-        document.getElementById('send-btn').disabled = true
-        await getSession(session)
-    }
-    //<script type="72b62d4313cfbc0894ca0765-text/javascript">
-    const socket = io(url + '/');
-    socket.on('msg', (msg) => {
-        if (msg) {
-            //document.getElementById('image').style.visibility = "hidden";
-            document.getElementById('send-btn').disabled = false
-            alert(msg.reason)
-        }
-
-    })
-
-    socket.on('qrCode', (qrCode) => {
-        let session = document.getElementById('session').value
-        if (session === qrCode.session) {
-
-            document.getElementById('image').src = qrCode.data
-        }
-    })
-
-    socket.on('whatsapp-status', (status) => {
-        if (status) {
-            alert('Whatsapp Aberto com sucesso')
-            document.getElementById('send-btn').disabled = false
-            document.getElementById('image').src = "./images/ok.png"
-        } else {
-            document.getElementById('send-btn').disabled = false
-            document.getElementById('image').src = "./images/error.png"
-        }
-    })
-}
-
-
-
+const host = 'http://localhost:3000'
 
 function addClient() {
     (async () => {
         const response = await fetch(
-            'https://api.spacewebso.com.br/tasks', {
+            `${host}/tasks`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -113,7 +28,7 @@ function addClient() {
 function sendMessage() {
     (async () => {
         const response = await fetch(
-            'https://api.spacewebso.com.br/tasks', {
+            `${host}/sendMessage`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -139,7 +54,7 @@ function sendMessage() {
 function schMessage() {
     (async () => {
         const response = await fetch(
-            'https://api.spacewebso.com.br/tasks', {
+            `${host}/schMessage`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -164,7 +79,7 @@ function schMessage() {
 }
 
 async function listClients() {
-    const response = await axios.get('https://api.spacewebso.com.br:3000/tasks')
+    const response = await axios.get(`${host}/tasks`)
     const clients = response.data
     const lista = document.getElementById('list-clients')
 
@@ -177,7 +92,7 @@ async function listClients() {
 }
 
 async function listSCHMessage() {
-    const response = await axios.get('https://api.spacewebso.com.br:3000/schMessage')
+    const response = await axios.get(`${host}/schMessage`)
     const clients = response.data
     const lista = document.getElementById('list-clients')
 
@@ -191,8 +106,6 @@ async function listSCHMessage() {
 
 async function deleteClient() {
     console.log(document.getElementById('number').value)
-    const response = await axios.delete(`https://api.spacewebso.com.br:3000/tasks/number/${document.getElementById("number").value}`)
+    const response = await axios.delete(`${host}/tasks/number/${document.getElementById("number").value}`)
     window.location.reload();
 }
-
-listSCHMessage()
